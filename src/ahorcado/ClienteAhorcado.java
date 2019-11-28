554c406f6b9fa5d83fb83f075f0793af49b488be
jose.guapache <jose.guapache@pc2813>
2019-11-28 14:07:48 +0100

    Terminar -.-

--- src/ahorcado/ClienteAhorcado.java
+++ src/ahorcado/ClienteAhorcado.java
@@ -13,42 +13,50 @@ public class ClienteAhorcado {
 
 	public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
 		// se crea la conexión
-		Scanner sc = new Scanner (System.in);
+		Scanner sc = new Scanner(System.in);
 		String host = "localhost";
 		Socket socket = new Socket(host, 2500); // conexión
-		
+
 		String palabra;
 		OutputStream ostream = socket.getOutputStream();
 		ObjectOutput s = new ObjectOutputStream(ostream);
-		
+		// Recibimos la palabra del servidor
 		ObjectInputStream istream = new ObjectInputStream(socket.getInputStream());
 		palabra = (String) istream.readObject();
-		//Recibimos la palabra y creamos el panel con el número de letras que tiene la palabra
-		
-		espacios (3);
+
+		// Creamos el panel con el número de letras que tiene la palabra
+		System.out.println("El juego va a terminar cuando tengas 7 errores");
+		int errores = 0;
+//		boolean habia = true;
+		espacios(3);
 		for (int i = 0; i < palabra.length(); i++) {
 			System.out.print("_" + " ");
 		}
+		espacios(2);
+		boolean terminado = false;
 		
-		System.out.println("Introduce la letra que crees que está");
-		String letra = sc.nextLine();
-		
-		if (letra.length() < 1) {
-			if (palabra.contains(letra)) {
-				for (int i = 0; i < palabra.length(); i++) {
-					System.out.print("_" + " ");
-				}
-			}
-		}
+		while (terminado == false) {
+			System.out.println("Introduce la letra que crees que está. Número de errores =" + errores);
+			String letra = sc.nextLine();
 
+			s.writeObject(letra);
+			s.flush();
+			String imprimir;
+			imprimir = (String) istream.readObject();
+			System.out.print(imprimir);
+			espacios (1);
+			
+		}
+		
+		sc.close();
 	}
-	
-	private static void espacios (int numLineas) {
+
+	private static void espacios(int numLineas) {
 		for (int i = 0; i < numLineas; i++) {
 			System.out.println();
 		}
 	}
-	
+
 //	private static void muñeco (boolean acierto) {
 //		if (acierto) {
 //			
--- src/ahorcado/ServidorAhorcado.java
+++ src/ahorcado/ServidorAhorcado.java
@@ -20,76 +20,108 @@ public class ServidorAhorcado {
 	public static void main(String[] args) {
 		ServerSocket serverAddr = null;
 		Socket sc = null;
-		InputStream istream =null;
-		ObjectInput in =null;
+		InputStream istream = null;
+		ObjectInput in = null;
 		try {
-			//SE CREA EL SERVIDOR
+			// SE CREA EL SERVIDOR
 			try {
-				serverAddr = new ServerSocket(2500); //Se crea el socket
+				serverAddr = new ServerSocket(2500); // Se crea el socket
 				sc = serverAddr.accept(); // esperando conexión
 				istream = sc.getInputStream();
 				in = new ObjectInputStream(istream);
-			}catch (Exception e){
+			} catch (Exception e) {
 				System.err.println("Error creando socket");
 			}
-			
-			//EMPIEZAN LOS PROCESOS
+
+			// EMPIEZAN LOS PROCESOS
 			try {
-				Path rutaFichero = Paths.get("/Servidor-Cliente Stream/src/ahorcado/palabras");
+				// Cargamos el fichero donde se encuentran las palabras
+				Path rutaFichero = Paths.get("../Servidor-Cliente Stream/src/ahorcado/palabras");
 				Charset charset = Charset.forName("ISO-8859-1");
 				BufferedReader br = null;
 				String palabras = null;
-					try {
-						br = Files.newBufferedReader(rutaFichero, charset);
-						palabras = br.readLine();
-						
-						br.close();
-						} catch (IOException x) {
-							System.err.format("IOException: %s%n", x);
-					}
-					
-					String [] arrayPalabras = new String [palabras.length()];
-					arrayPalabras = palabras.split(" ");
-					int num = (int) (Math.random() * (palabras.length() - 1) + 1);
-					
-					String palabra = arrayPalabras [num];
-					
-					ObjectOutputStream ostream = new ObjectOutputStream(sc.getOutputStream());
-					ostream.writeObject(palabra);
-					ostream.flush();
-					
-					
-				String texto= (String) in.readObject();
+				try {
+					br = Files.newBufferedReader(rutaFichero, charset);
+					palabras = br.readLine();
 
-				Path ruta = null;
-				ruta = Paths.get("../Servidor-Cliente Stream/src/texto/ficheroCreado");
+					br.close();
+				} catch (IOException x) {
+					System.err.format("IOException: %s%n", x);
+				}
+				// Metemos todas las palabras en un array
+				String[] arrayPalabras = new String[palabras.length()];
+				// Cortamos las palabras cuando encuentre un espacio y lo metemos en un array
+				// para que cada palabra tenga un id
+				arrayPalabras = palabras.split(" ");
+				// Generamos un número aleatorio
+				int num = (int) (Math.random() * (1 - arrayPalabras.length + 1) + arrayPalabras.length);
+				// Metemos una palabra aleatoria con el índice aleatorio que generamos
+				// anteriormente
+				String palabra = arrayPalabras[num];
+				// Enviamos la palabra al cliente
+				ObjectOutputStream ostream = new ObjectOutputStream(sc.getOutputStream());
+				ostream.writeObject(palabra);
+				ostream.flush();
+//				int errores = 0;
+//				boolean habia = false;
+				String enviar = " ";
+				while (true) {
+					String letra = (String) in.readObject();
+					char l = letra.charAt(0);
+					char[] array = new char[palabra.length()];
 
-				if (ruta.toFile().exists()) {
-					try (BufferedWriter wr = Files.newBufferedWriter(ruta, charset, StandardOpenOption.APPEND)) {
-						wr.write(" " + texto);
-						
-					}catch (IOException e) {
-						e.printStackTrace();
+					for (int i = 0; i < palabra.length(); i++) {
+						array[i] = palabra.charAt(i);
 					}
-				}else {
-					if (ruta.toFile().createNewFile()) {
-						try (BufferedWriter wr = Files.newBufferedWriter(ruta, charset, StandardOpenOption.APPEND)) {
-								wr.write(texto);
-								
-							}catch (IOException e) {
-								e.printStackTrace();
-							}
+
+					for (int j = 0; j < array.length; j++) {
+						if (l == array[j]) {
+							enviar = letra;
+						} else {
+							enviar = " _ ";
 						}
 					}
-				}catch(Exception e) {
-					System.err.println("excepcion " + e.toString() );
-					e.printStackTrace() ;
+
+					ostream.writeObject(enviar);
+					ostream.flush();
 				}
-		}catch (Exception e) {
-			System.err.println("excepcion " + e.toString() );
-			e.printStackTrace() ;
+			} catch (Exception e) {
+				System.err.println("excepcion " + e.toString());
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			System.err.println("excepcion " + e.toString());
+			e.printStackTrace();
 		}
-		
 	}
 
 }
+
+//	Path ruta = null;
+//	ruta = Paths.get("../Servidor-Cliente Stream/src/texto/ficheroCreado");
+//
+//	if (ruta.toFile().exists()) {
+//		try (BufferedWriter wr = Files.newBufferedWriter(ruta, charset, StandardOpenOption.APPEND)) {
+//			wr.write(" " + texto);
+//			
+//		}catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}else {
+//		if (ruta.toFile().createNewFile()) {
+//			try (BufferedWriter wr = Files.newBufferedWriter(ruta, charset, StandardOpenOption.APPEND)) {
+//					wr.write(texto);
+//					
+//				}catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//	}catch(Exception e) {
+//		System.err.println("excepcion " + e.toString() );
+//		e.printStackTrace() ;
+//	}
+//}catch (Exception e) {
+//System.err.println("excepcion " + e.toString() );
+//e.printStackTrace() ;
+//}
